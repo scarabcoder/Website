fc5fc9e5aed48a78473b71ec4ca1eb8d82432a8f
Nicholas Harris <scarabcoder@gmail.com>
2022-11-09 19:22:23 -0800

    Updated

--- _next/static/TqrLkm3D4q7doNWXy9l4W/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/621-444c0885a2857f79.js","static/chunks/pages/index-dc515beba10778a3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
